d3fea05f01bda734dcc79d4630910f05942b2191
mcdnnll <michael@mcdnnll.com>
2016-01-09 21:47:27 +1100

    Removed promise constructors from server

--- src/server/dao/dao.prod.js
+++ src/server/dao/dao.prod.js
@@ -1,60 +1,61 @@
 const _ = require('lodash');
 const models = require('../models');
+const logger = require('../utils/logger');
 const sql = require('../utils/preparedSql');
 const getTime = require('../utils/getTime');
 const prd = require('../utils/processRunData');
 
 exports.getRunCountData = () => {
 
-  return new Promise((resolve, reject) => {
+  logger.log('info', 'getRunCountData(): Starting exec');
 
-    const db = models.sequelize;
+  const db = models.sequelize;
 
-    const runCountData = db.query(sql.getRunCountData, {model: db.Entries}).spread((results) => results);
+  // Get current time info to calculate run data for the relative period
+  const timePeriods = getTime();
 
-    runCountData.then((dbData, dbErr) => {
-      if (dbErr) reject(dbErr);
+  // Query DB for latest running count aggregates
+  db.query(sql.getRunCountData, {model: db.Entries}).spread((results) => results)
 
-      // Get current time periods to calculate relevant run count
-      const timePeriods = getTime();
-
-      prd.calcRunCount(timePeriods, dbData).then((rcRes, rcErr) => {
-        if (rcErr) reject(rcErr);
-        resolve(rcRes);
-      });
-
-    });
-  });
+    // Calculate run count totals relevant to current time period
+    .then((dbData) => prd.calcRunCount(timePeriods, dbData))
+    .then((runCountData) => {
+      logger.log('info', 'getRunCountData(): Returning');
+      return runCountData;
+    })
+    .catch((e) => e);
 };
 
 exports.getBestRunData = () => {
-  return new Promise((resolve, reject) => {
+  logger.log('info', 'getBestRunData(): Starting exec');
 
-    const db = models.sequelize;
+  const db = models.sequelize;
 
-    // TODO: Optimise sql queries
-    const q1 = db.query(sql.getBestRunThisWeek, {model: db.Entries}).spread((results) => results);
-    const q2 = db.query(sql.getBestRunLastWeek, {model: db.Entries}).spread((results) => results);
-    const q3 = db.query(sql.getBestRunThisMonth, {model: db.Entries}).spread((results) => results);
-    const q4 = db.query(sql.getBestRunLastMonth, {model: db.Entries}).spread((results) => results);
-    const q5 = db.query(sql.getBestRunThisYear, {model: db.Entries}).spread((results) => results);
-    const q6 = db.query(sql.getBestRunLastYear, {model: db.Entries}).spread((results) => results);
+  // TODO: Optimise sql queries
+  const q1 = db.query(sql.getBestRunThisWeek, {model: db.Entries}).spread((results) => results);
+  const q2 = db.query(sql.getBestRunLastWeek, {model: db.Entries}).spread((results) => results);
+  const q3 = db.query(sql.getBestRunThisMonth, {model: db.Entries}).spread((results) => results);
+  const q4 = db.query(sql.getBestRunLastMonth, {model: db.Entries}).spread((results) => results);
+  const q5 = db.query(sql.getBestRunThisYear, {model: db.Entries}).spread((results) => results);
+  const q6 = db.query(sql.getBestRunLastYear, {model: db.Entries}).spread((results) => results);
 
-    Promise.all([q1, q2, q3, q4, q5, q6]).then((dbData, dbErr) => {
-      if (dbErr) reject(dbErr);
+  return Promise.all([q1, q2, q3, q4, q5, q6]).then((dbData) => {
 
-      const bestRunData = _.flatten(dbData);
+    // Handle case where no run data is available for a certain time period
+    // Allows for consistency on the client
+    const defaultResult = {value: 0, date: 0};
 
-      const bestRunSummary = {
-        thisWeek: bestRunData[0],
-        lastWeek: bestRunData[1],
-        thisMonth: bestRunData[2],
-        lastMonth: bestRunData[3],
-        thisYear: bestRunData[4],
-        lastYear: bestRunData[5],
-      };
+    const bestRunSummary = {
+      thisWeek: dbData[0][0] || defaultResult,
+      lastWeek: dbData[1][0] || defaultResult,
+      thisMonth: dbData[2][0] || defaultResult,
+      lastMonth: dbData[3][0] || defaultResult,
+      thisYear: dbData[4][0] || defaultResult,
+      lastYear: dbData[5][0] || defaultResult,
+    };
 
-      resolve(bestRunSummary);
-    });
-  });
+    logger.log('info', 'getBestRunData(): Returning');
+    return bestRunSummary;
+  })
+  .catch((e) => e);
 };
--- src/server/utils/preparedSql.js
+++ src/server/utils/preparedSql.js
@@ -10,67 +10,67 @@ ORDER BY yr, mnth;
 `;
 
 exports.getBestRunThisYear = `
-SELECT "date", max("runningIndex") as bestrunindex
+SELECT "date", max("runningIndex") as value
 FROM "Entries"
 WHERE "date"
 BETWEEN date_trunc('year', current_date)
 AND current_date
 GROUP by "date"
-ORDER BY bestrunindex DESC
+ORDER BY value DESC
 LIMIT 1;
 `;
 
 exports.getBestRunLastYear = `
-SELECT "date", max("runningIndex") as bestrunindex
+SELECT "date", max("runningIndex") as value
 FROM "Entries"
 WHERE "date"
 BETWEEN date_trunc('year', current_date - interval '1 year')
 AND date_trunc('year', current_date)
 GROUP by "date"
-ORDER BY bestrunindex DESC
+ORDER BY value DESC
 LIMIT 1;
 `;
 
 exports.getBestRunThisMonth = `
-SELECT "date", max("runningIndex") as bestrunindex
+SELECT "date", max("runningIndex") as value
 FROM "Entries"
 WHERE "date"
 BETWEEN date_trunc('month', current_date)
 AND current_date
 GROUP by "date"
-ORDER BY bestrunindex, "date" DESC
+ORDER BY value, "date" DESC
 LIMIT 1;
 `;
 
 exports.getBestRunLastMonth = `
-SELECT "date", max("runningIndex") as bestrunindex
+SELECT "date", max("runningIndex") as value
 FROM "Entries"
 WHERE "date"
 BETWEEN date_trunc('month', current_date - interval '1 month')
 AND date_trunc('month', current_date)
 GROUP by "date"
-ORDER BY bestrunindex, "date" DESC
+ORDER BY value, "date" DESC
 LIMIT 1;
 `;
 
 exports.getBestRunThisWeek = `
-SELECT "date", max("runningIndex") as bestrunindex
+SELECT "date", max("runningIndex") as value
 FROM "Entries"
 WHERE "date"
 BETWEEN date_trunc('week', current_date)
 AND current_date
 GROUP by "date"
-ORDER BY bestrunindex, "date" DESC
+ORDER BY value, "date" DESC
 LIMIT 1;
 `;
 
 exports.getBestRunLastWeek = `
-SELECT "date", max("runningIndex") as bestrunindex
+SELECT "date", max("runningIndex") as value
 FROM "Entries"
 WHERE "date"
 BETWEEN date_trunc('week', current_date - interval '1 week')
 AND date_trunc('week', current_date)
 GROUP by "date"
-ORDER BY bestrunindex, "date" DESC
+ORDER BY value, "date" DESC
 LIMIT 1;
 `;
